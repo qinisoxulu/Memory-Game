1677934c8baa00180499dc3e11e7cfbd350d68f8
Qiniso Xulu <qiniso.nhlakanipho.xulu@gmail.com>
2025-03-05 10:24:20 +0200

    Moving connection from client.py to .env

--- backend/connection/client.py
+++ backend/connection/client.py
@@ -1,11 +1,13 @@
 import socket
+import os
 
+host = os.getenv('HOST', 'localhost')
+port = int(os.getenv('PORT', 12345))
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-host = 'localhost'
-port = 12345
-s.connect((host, port))
-s.send(b'Hello, server!')
-data = s.recv(1024)
-print('Received:', repr(data))
-s.close()
+try:
+    s.connect((host, port))
+    s.send(b'Hello, server!')
+    data = s.recv(1024)
+    print('Received:', repr(data))
+finally:
+    s.close()
